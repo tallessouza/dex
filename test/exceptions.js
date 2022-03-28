6ca2fbd8fcf6de19f80b351118e3f67ec7282c16
tallessouza <talles-swd@live.com>
2022-03-28 20:46:54 -0300

    Exception file created to get errors

---
+++ test/exceptions.js
@@ -0,0 +1,61 @@
+const PREFIX = 'Returned error: VM Exception while processing transaction: '
+
+async function tryCatch(promise, message) {
+  try {
+    await promise
+    throw null
+  } catch (error) {
+    assert(error, 'Expected an error but did not get one')
+    assert(
+      error.message.startsWith(PREFIX + message),
+      "Expected an error starting with '" +
+        PREFIX +
+        message +
+        "' but got '" +
+        error.message +
+        "' instead"
+    )
+  }
+}
+async function tryCatch2(promise, message) {
+  try {
+    await promise
+    throw null
+  } catch (error) {
+    assert(error, 'Expected an error but did not get one')
+    assert(
+      error.message.startsWith(message),
+      "Expected an error starting with '" +
+        message +
+        "' but got '" +
+        error.message +
+        "' instead"
+    )
+  }
+}
+module.exports = {
+  catchRevert: async function (promise) {
+    await tryCatch(promise, 'revert')
+  },
+  catchOutOfGas: async function (promise) {
+    await tryCatch(promise, 'out of gas')
+  },
+  catchInvalidJump: async function (promise) {
+    await tryCatch(promise, 'invalid JUMP')
+  },
+  catchInvalidOpcode: async function (promise) {
+    await tryCatch(promise, 'invalid opcode')
+  },
+  catchStackOverflow: async function (promise) {
+    await tryCatch(promise, 'stack overflow')
+  },
+  catchStackUnderflow: async function (promise) {
+    await tryCatch(promise, 'stack underflow')
+  },
+  catchStaticStateChange: async function (promise) {
+    await tryCatch(promise, 'static state change')
+  },
+  catchInvalidAddress: async function (promise) {
+    await tryCatch2(promise, 'invalid address')
+  }
+}
