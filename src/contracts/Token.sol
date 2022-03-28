2b650462e60f94ac41b79fedc0eabae461d71296
tallessouza <talles-swd@live.com>
2022-03-28 20:45:30 -0300

    SafeMath lib added and event created

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Token {
+    using SafeMath for uint256;
+
     string public name = "Token";
     string public symbol = "Symbol";
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor() {
         totalSupply = 1000000 * (10**decimals);
@@ -18,8 +23,11 @@ contract Token {
         public
         returns (bool success)
     {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 }
