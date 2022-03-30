07b842d89e72f8c53b974df8e17bfb04c21ec64b
tallessouza <talles-swd@live.com>
2022-03-29 22:17:04 -0300

    Delegated token transfer and test

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable jest/valid-describe-callback */
 const Token = artifacts.require('./Token')
 const { catchRevert, catchInvalidAddress } = require('./exceptions')
 
@@ -6,13 +5,14 @@ const tokens = n => {
   return new web3.utils.BN(web3.utils.toWei(n.toString(), 'ether'))
 }
 
-contract('Token', ([deployer, receiver]) => {
+contract('Token', ([deployer, receiver, exchange]) => {
   const name = 'Token'
   const symbol = 'Symbol'
   const decimals = '18'
   const totalSupply = tokens(1000000).toString()
   let token
   let result
+  let amount
 
   beforeEach(async () => {
     token = await Token.new()
@@ -42,9 +42,7 @@ contract('Token', ([deployer, receiver]) => {
   })
 
   describe('token transfer', () => {
-    let amount
-
-    describe('success', async () => {
+    describe('success', () => {
       beforeEach(async () => {
         amount = tokens(100)
         result = await token.transfer(receiver, amount, { from: deployer })
@@ -65,7 +63,6 @@ contract('Token', ([deployer, receiver]) => {
       it('emits a transfer event', async () => {
         const log = result.logs[0]
         assert.equal(log.event, 'Transfer', 'Incorrect')
-
         const event = log.args
         assert.equal(event.from.toString(), deployer, 'Incorrect')
         assert.equal(event.to, receiver, 'Incorrect')
@@ -73,7 +70,7 @@ contract('Token', ([deployer, receiver]) => {
       })
     })
 
-    describe('failure', async () => {
+    describe('failure', () => {
       it('rejects insufficient balances', async () => {
         let invalidAmount
         invalidAmount = tokens(100000000) // 100 million - greater than total supply
@@ -93,6 +90,96 @@ contract('Token', ([deployer, receiver]) => {
           token.transfer('0x0', amount, { from: deployer })
         )
       })
+    })
+  })
+  describe('approve tokens', () => {
+    beforeEach(async () => {
+      amount = tokens(100)
+      result = await token.approve(exchange, amount, { from: deployer })
+    })
+    describe('success', () => {
+      it('allocate allowance', async () => {
+        const allowance = await token.allowance(deployer, exchange)
+        assert.equal(allowance.toString(), amount.toString())
+      })
+
+      it('emits a transfer event', async () => {
+        const log = result.logs[0]
+        assert.equal(log.event, 'Approval', 'Incorrect')
+        const event = log.args
+        assert.equal(event.owner.toString(), deployer, 'Incorrect')
+        assert.equal(event.spender, exchange, 'Incorrect')
+        assert.equal(event.value.toString(), amount.toString(), 'Incorrect')
+      })
+    })
+    describe('failure', () => {
+      it('reject invalid spender', async () => {
+        await catchInvalidAddress(
+          token.approve(0x0, amount, { from: deployer })
+        )
+      })
+    })
+  })
+
+  describe('token transfer', () => {
+    beforeEach(async () => {
+      amount = tokens(100)
+      await token.approve(exchange, amount, { from: deployer })
+    })
+
+    describe('success', () => {
+      beforeEach(async () => {
+        amount = tokens(100)
+        result = await token.transferFrom(deployer, receiver, amount, {
+          from: exchange
+        })
+      })
+
+      it('transfers token balances', async () => {
+        let balanceOf
+        balanceOf = await token.balanceOf(deployer)
+        assert.equal(
+          balanceOf.toString(),
+          tokens(999900).toString(),
+          'Incorrect'
+        )
+        balanceOf = await token.balanceOf(receiver)
+        assert.equal(balanceOf.toString(), tokens(100).toString(), 'Incorrect')
+      })
+
+      it('reset allowance', async () => {
+        const allowance = await token.allowance(deployer, exchange)
+        assert.equal(allowance.toString(), '0')
+      })
+
+      it('emits a transfer event', async () => {
+        const log = result.logs[0]
+        assert.equal(log.event, 'Transfer', 'Incorrect')
+        const event = log.args
+        assert.equal(event.from.toString(), deployer, 'Incorrect')
+        assert.equal(event.to, receiver, 'Incorrect')
+        assert.equal(event.value.toString(), amount.toString(), 'Incorrect')
+      })
+    })
+
+    describe('failure', () => {
+      // it('rejects insufficient balances', async () => {
+      //   let invalidAmount
+      //   invalidAmount = tokens(100000000) // 100 million - greater than total supply
+      //   await catchRevert(
+      //     token.transfer(receiver, invalidAmount, { from: deployer })
+      //   )
+      //   // Attempt transfer tokens, when you have none
+      //   invalidAmount = tokens(10) // recipient has no tokens
+      //   await catchRevert(
+      //     token.transfer(deployer, invalidAmount, { from: receiver })
+      //   )
+      // })
+      // it('rejects invalid recipients', async () => {
+      //   await catchInvalidAddress(
+      //     token.transfer('0x0', amount, { from: deployer })
+      //   )
+      // })
     })
   })
 })
