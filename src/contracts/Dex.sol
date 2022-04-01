7ce7c5bc41638579b8877d3e1462a7d0a8a53bf4
tallessouza <talles-swd@live.com>
2022-03-31 21:52:49 -0300

    Deposit improvements

--- src/contracts/Dex.sol
+++ src/contracts/Dex.sol
@@ -1,18 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "./Token.sol";
 
 contract Dex {
+    using SafeMath for uint256;
+
     address public feeAccount;
     uint256 public feePercent;
+    address constant ETHER = address(0);
+
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
+    function depositEther() public payable {
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+    }
+
     function depositToken(address _token, uint256 _amount) public {
-        Token(_token).transferFrom(msg.sender, address(this), _amount);
+        require(_token != ETHER);
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 }
